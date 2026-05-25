4d661613af5ec290425604cf40b508885a5bcdec
Bilash <bilashchandra.das@davita.com>
2026-05-25 19:24:25 +0530

    feat: PI-BE

--- src/HealthPlatform.Tests/obj/Debug/net8.0/HealthPlatform.Tests.AssemblyInfo.cs
+++ src/HealthPlatform.Tests/obj/Debug/net8.0/HealthPlatform.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthPlatform.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16dc7ecc4a970df28c700631281983ad26490d58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60dd16d56e88c48445d6e2689e03af615174515")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthPlatform.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthPlatform.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
